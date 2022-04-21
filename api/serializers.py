060e8f9ef7ea54c7b0bae64041dd24d1e1efe471
hoshimotoyasunori <hoshimotoyasunori@gmail.com>
2022-04-21 14:59:41 +0900

    edit serializer2

--- api/serializers.py
+++ api/serializers.py
@@ -14,6 +14,7 @@ class TargetSerializer(serializers.ModelSerializer):
         model = Target
         fields = (
             # 'user_id',
+            'user',
             'id',
             'target1_1',
             'target1_2',
